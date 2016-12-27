0e3b45a6b3b5bfef35184dc4ec3b48ec2cef8820
Andrii Vasyliev <sol@hiqdev.com>
2016-12-27 13:56:14 +0000

    csfixed

--- src/menus/OldPanelMenu.php
+++ src/menus/OldPanelMenu.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * HiPanel module for MRDP compatibility
+ *
+ * @link      https://github.com/hiqdev/hipanel-mrdp
+ * @package   hipanel-mrdp
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2016, HiQDev (http://hiqdev.com/)
+ */
 
 namespace hipanel\mrdp\menus;
 
--- src/messages/ru/ahnames:hipanel.php
+++ src/messages/ru/ahnames:hipanel.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * HiPanel module for MRDP compatibility
+ *
+ * @link      https://github.com/hiqdev/hipanel-mrdp
+ * @package   hipanel-mrdp
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2016, HiQDev (http://hiqdev.com/)
+ */
 
 return [
     'Return to old panel' => 'Вернуться в старую панель',
